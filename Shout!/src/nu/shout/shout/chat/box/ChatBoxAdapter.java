da6ac4fc3865ae40ccc9971fa97421ce0706f750
Milan Boers <milan.boers@gmail.com>
2014-01-11 18:17:35 +0100

    Smileys toegevoegd

--- Shout!/src/nu/shout/shout/chat/box/ChatBoxAdapter.java
+++ Shout!/src/nu/shout/shout/chat/box/ChatBoxAdapter.java
@@ -1,6 +1,11 @@
 package nu.shout.shout.chat.box;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import nu.shout.shout.R;
 import nu.shout.shout.chat.items.Chat;
@@ -8,6 +13,8 @@ import nu.shout.shout.chat.items.Error;
 import nu.shout.shout.chat.items.Item;
 import nu.shout.shout.chat.items.Report;
 import android.content.Context;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,9 +26,13 @@ public class ChatBoxAdapter extends BaseAdapter {
 	
 	private List<Item> items;
 	
+	protected Map<String, Integer> smiley_map = new HashMap<String, Integer>();
+	
 	public ChatBoxAdapter(Context context, List<Item> items) {
 		this.ctx = context;
 		this.items = items;
+		
+		this.smiley_map.put(":-?\\)", R.drawable.smiley);
 	}
 	
 	@Override
@@ -43,9 +54,27 @@ public class ChatBoxAdapter extends BaseAdapter {
 		LayoutInflater vi = LayoutInflater.from(this.ctx);
 		v = vi.inflate(R.layout.list_chat_chat, null);
 		
-		TextView msg = (TextView) v.findViewById(R.id.list_chat_chat_message);
-		msg.setText("<" + i.nickname + "> " + i.message);
+		TextView nick = (TextView) v.findViewById(R.id.list_chat_chat_nickname);
+		// TODO: in layout scheiden
+		nick.setText(i.nickname + ":" + " ");
 		
+		TextView msg = (TextView) v.findViewById(R.id.list_chat_chat_message);
+		msg = setChatToTextView(msg, i.message);
+		
+		return v;
+	}
+	
+	private TextView setChatToTextView(TextView v, String chatText) {
+		SpannableString text = new SpannableString(chatText);
+		
+		for(Entry<String, Integer> kvp : this.smiley_map.entrySet()) {
+			Pattern pattern = Pattern.compile(kvp.getKey());
+			Matcher matcher = pattern.matcher(chatText);
+			while(matcher.find()) {
+				text.setSpan(new ImageSpan(this.ctx, kvp.getValue()), matcher.start(), matcher.end(), 0);
+			}
+		}
+		v.setText(text);
 		return v;
 	}
 	
